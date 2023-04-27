c33f019ad7e50d1e31ef4275a2595e850a399023
teylorsmith34 <teylorsmith34@gmail.com>
2023-04-27 15:46:36 -0400

    Added some BootStrap and Font styling.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,22 @@
 // Create a HTML page for the quiz. DONE
 
-// Add a start button to the page. this will statr the quiz, render the first question and start the timer
+// Add a start button to the page. this will statr the quiz, render the first question and start the timer DONE
 
-// Add a question and answer section to the page.
+// Add a question and answer section to the page. DONE
 
-// Add a timer to the page write JavaScript code to start the timer when the start button is clicked.
+// Add a timer to the page write JavaScript code to start the timer when the start button is clicked.DONE
 
-// Write JavaScript code to present the next question when a question is answered CORRECTLY(?)
+// Write JavaScript code to present the next question when a question is answered CORRECTLY(?)DONE
 
 // Write JavaScript code to subtract time from the timer when a question is answered incorrectly. Do not progress to next question (?)
 
 // Write JavaScript code to check if the game is over when all questions are answered or the timer reaches 0.
 
 // Write JavaScript code to save the user's initials and score when the game is over.
+
+// create new html page with its own js for highscores (it's probably gonna be easier)
+
+// use this to save/retrieve highscores. localStorage.getItem(), localStorage.setItem(). You will also need JSON.stringify() and JSON.parse()
 
 const timer = document.getElementById("timer");
 const startContainer = document.getElementById("start-container");
@@ -60,16 +64,41 @@ const questions = [
   // },
 ];
 
-function renderQuestion(currentIndex) {
-  questionHeader.innerText = questions[currentIndex].question;
+function renderNextQuestion() {
+  console.log("correct");
+  questionHeader.innerHTML = "";
+  answersContainer.innerHTML = "";
+  currentIndex++;
+  renderQuestion(currentIndex);
+}
 
+function renderButtonsAndHandleAnswer(answerArray, correctAnswer) {
   // create answer buttons
-  for (let i = 0; i < questions[currentIndex].answers.length; i++) {
+  for (let i = 0; i < answerArray.length; i++) {
     const answerBtn = document.createElement("button");
-    answerBtn.innerText = questions[currentIndex].answers[i];
+    answerBtn.innerText = answerArray[i];
+    answerBtn.setAttribute("data-index", i);
+    answerBtn.addEventListener("click", function (event) {
+      //it is going to check if the data attribute from the element matches the correctAnswer value
+      const chosenAnswer = Number(event.target.getAttribute("data-index"));
+
+      if (chosenAnswer === correctAnswer) {
+        renderNextQuestion();
+      } else {
+        console.log("wrong");
+      }
+    });
 
     answersContainer.appendChild(answerBtn);
   }
+}
+
+function renderQuestion(currentIndex) {
+  questionHeader.innerText = questions[currentIndex].question;
+  renderButtonsAndHandleAnswer(
+    questions[currentIndex].answers,
+    questions[currentIndex].correctAnswer
+  );
 }
 
 function submitAnswer() {
